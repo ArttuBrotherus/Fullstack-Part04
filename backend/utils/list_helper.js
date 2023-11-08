b95d79fe7cf3fe4abe0d48da4f2c298946379158
ArttuBrotherus <44648394+ArttuBrotherus@users.noreply.github.com>
2023-11-08 15:52:00 +0200

    Chap. 04 ex. 04

--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -1,7 +1,26 @@
 const dummy = (blogs) => {
     return 1
   }
+
+function addition(total, newer){
+    return total + newer
+}
+
+function totalLikes(blogs){
+
+    if(blogs.length == 0){
+        return 0
+    }
+
+    const blogLikes = []  
+    for (let i = 0; i < blogs.length; i++) {
+        blogLikes.push(blogs[i].likes);
+   }
+
+   return blogLikes.reduce(addition)
+}
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }

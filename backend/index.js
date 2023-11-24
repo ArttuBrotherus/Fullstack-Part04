144d38eb17f1412ddd6b5278cda392b6d69578b1
ArttuBrotherus <44648394+ArttuBrotherus@users.noreply.github.com>
2023-11-24 12:51:37 +0200

    Chap. 04 ex. 18

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,38 @@ const Blog = require('./models/blog')
 const { getAll, create, deleteBlog } = require('./models/blogSer')
 const bcrypt = require('bcrypt')
 const User = require('./models/user')
+const jwt = require('jsonwebtoken')
 
 const app = express()
 app.use(express.json())
 app.use(cors())
+
+
+app.post('/api/login', async (request, response) => {
+  const { username, password } = request.body
+
+  const user = await User.findOne({ username })
+  const passwordCorrect = user === null
+    ? false
+    : await bcrypt.compare(password, user.passwordHash)
+
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: 'invalid username or password'
+    })
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  }
+
+  const token = jwt.sign(userForToken, process.env.SECRET)
+
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name })
+})
 
 app.get('/api/users', async (request, response) => {
   const users = await User
